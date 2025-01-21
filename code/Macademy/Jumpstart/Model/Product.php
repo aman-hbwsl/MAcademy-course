5122522ed11b129c1f478829bd2f211b2d8ef564
Aman Semwal <aman.semwal@hbwsl.com>
2025-01-21 14:26:36 +0530

    Add constructor property promotion

--- code/Macademy/Jumpstart/Model/Product.php
+++ code/Macademy/Jumpstart/Model/Product.php
@@ -5,11 +5,9 @@ declare(strict_types=1);
 namespace Macademy\Jumpstart\Model;
 
 class Product{
-    private Category $category;
-
-    function __construct(Category $category){
-        $this->category = $category;
-    }
+    function __construct(
+        private Category $category
+    ){}
 
     function getCategoryName(){
         return $this->category->getName();
