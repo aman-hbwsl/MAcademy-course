689f6e4947eaa095578e4d058d2c10393539a4ad
Aman Semwal <aman.semwal@hbwsl.com>
2025-01-21 15:45:45 +0530

    Add standard page result

--- code/Macademy/Jumpstart/Controller/Index/Index.php
+++ code/Macademy/Jumpstart/Controller/Index/Index.php
@@ -4,11 +4,19 @@ declare(strict_types= 1);
 
 namespace Macademy\Jumpstart\Controller\Index;
 
+use Magento\Framework\View\Result\PageFactory;
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
 
 class Index implements HttpGetActionInterface{
-    public function execute(){
-        die('Jumpstart!');
+    private $pageFactory;
+
+    public function __construct(PageFactory $pageFactory){
+        $this->pageFactory = $pageFactory;
+    }
+
+    public function execute(): Page{
+        return $this->pageFactory->create();
     }
 }
 
